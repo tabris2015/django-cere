1de479d63e76b2146a4c609a574da33ac532e612
Jose Laruta <jose.laruta@coxautoinc.com>
2021-06-02 21:33:05 -0400

    models demo

---
+++ bank/clientes/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha_registro = models.DateTimeField()
+
+    def __str__(self):
+        return self.nombre
+
+
+class Cuenta(models.Model):
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    categoria = models.CharField(max_length=30, default='normal')
+    balance = models.IntegerField()
