a31eb475853d5631965b7909e585c84a2f5ef577
Jose Laruta <jose.laruta@coxautoinc.com>
2021-06-07 20:14:06 -0400

    modelo tarea

--- tareas/lista_tareas/models.py
+++ tareas/lista_tareas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Tarea(models.Model):
+    texto = models.CharField(max_length=300)
+    prioridad = models.IntegerField(default=0)
+    fecha_creacion = models.DateTimeField()
