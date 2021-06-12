d67595f9a71340c5be68f688507982314b4a4288
Jose Laruta <jose.laruta@coxautoinc.com>
2021-06-11 21:27:14 -0400

    inventario class based views

--- almacen/inventario/urls.py
+++ almacen/inventario/urls.py
@@ -1,8 +1,39 @@
 from django.urls import path
-from . import views
+from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required, permission_required
+from .views import ItemView, CategoriaView, EncargadosListView, ItemDetailView, EncargadoDetailView
 
 
 app_name = 'inventario'
 urlpatterns = [
-    path('items/', views.items, name='items'),
+    path(
+        '',
+        TemplateView.as_view(template_name='inventario/index.html'),
+        name='index'
+    ),
+    path(
+        'items/',
+        login_required(ItemView.as_view()),
+        name='items'
+    ),
+    path(
+        'categorias/',
+        permission_required('inventario.add_categoria')(CategoriaView.as_view()),
+        name='categorias'
+    ),
+    path(
+        'encargados/',
+        EncargadosListView.as_view(),
+        name='encargados'
+    ),
+    path(
+        'items/detalle/<slug:slug>/',
+        ItemDetailView.as_view(),
+        name='detalle_item'
+    ),
+    path(
+        'encargados/detalle/<slug:slug>/',
+        EncargadoDetailView.as_view(),
+        name='detalle_encargado'
+    ),
 ]
--- almacen/inventario/views.py
+++ almacen/inventario/views.py
@@ -1,10 +1,68 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import Item
-from .forms import ItemForm
+from django.views import View
+from django.views.generic import ListView, DetailView
+from .models import Item, Categoria, Encargado
+from .forms import ItemForm, CategoriaForm
 
 
+class EncargadosListView(ListView):
+    model = Encargado
+    context_object_name = 'encargados'
+
+
+class CrearYListarView(View):
+    name = 'objects'
+    template = 'default'
+    form = None
+    model = None
+
+    def get(self, request):
+        return render(
+            request,
+            self.template,
+            {
+                self.name: self.model.objects.all(),
+                'form': self.form()
+            }
+        )
+
+    def post(self, request):
+        form = self.form(request.POST)
+        form.save()
+        return HttpResponseRedirect(reverse(f'inventario:{self.name}'))
+
+
+class ItemView(CrearYListarView):
+    name = 'items'
+    template = 'inventario/items.html'
+    form = ItemForm
+    model = Item
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = 'inventario/detalle_item.html'
+    slug_field = 'id'
+
+
+class EncargadoDetailView(DetailView):
+    model = Encargado
+    template_name = 'inventario/detalle_encargado.html'
+    slug_field = 'id'
+
+
+class CategoriaView(CrearYListarView):
+    name = 'categorias'
+    template = 'inventario/categorias.html'
+    form = CategoriaForm
+    model = Categoria
+
+
+
+@login_required
 def items(request):
     if request.method == 'POST':
         form = ItemForm(request.POST)
